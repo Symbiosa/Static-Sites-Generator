38b565af23d132f17e9334a6db5d7ab6d97af877
Symbiosa <103031295+Symbiosa@users.noreply.github.com>
2024-06-17 23:06:55 +0300

    quick redo

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -5,10 +5,10 @@ from htmlnode import HTMLNode
 
 class TestHTMLNode(unittest.TestCase):
     def test_eq(self):
-        node = HTMLNode("This is a text node", "bold", "www.hyvinvointvarkkaja.fi")
-        node2 = HTMLNode("Node is text", "italic", url=None)
-        node3 = HTMLNode("String", "Kissa", url = False)
-        node4 = HTMLNode(self,self,url = self)
+        node = HTMLNode("<a>", "21", "many", "any")
+        node2 = HTMLNode("Node is text", "italic", None, "catcat")
+        node3 = HTMLNode("String", "Kissa", False, "Gotgot")
+        node4 = HTMLNode(self,self,self, "cat")
         self.assertEqual(node, node2)
         self.assertEqual(node,node3)
         self.assertEqual(node,node4)
