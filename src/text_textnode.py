f6ed31e40f02a78a4a2060a44253a2cbef1b3b28
Symbiosa <103031295+Symbiosa@users.noreply.github.com>
2024-06-17 22:44:21 +0300

    Hmmmm2

--- src/text_textnode.py
+++ src/text_textnode.py
@@ -8,9 +8,10 @@ class TestTextNode(unittest.TestCase):
         node = TextNode("This is a text node", "bold", "www.hyvinvointvarkkaja.fi")
         node2 = TextNode("Node is text", "italic", url=None)
         node3 = TextNode("String", "Kissa", url = False)
+        node4 = TextNode(self,self,url = self)
         self.assertEqual(node, node2)
         self.assertEqual(node,node3)
-
+        self.assertEqual(node,node4)
 
 if __name__ == "__main__":
     unittest.main()
