f126264658325f8969119f8c38a48458ffd08b53
Symbiosa <103031295+Symbiosa@users.noreply.github.com>
2024-06-17 22:06:15 +0300

    Me commit change

---
+++ src/textnode.py
@@ -0,0 +1,12 @@
+class TextNode:
+    def __init__(self,text,text_type,url):
+        self.text = text
+        self.text_type = text_type
+        self.url = url
+    
+    def __eq__(self):
+        return (self.text) == (self.text_type) == (self.url)
+    
+    def __repr__(self):
+        return f"{self.text}{self.text_type}{self.url}"
+        
