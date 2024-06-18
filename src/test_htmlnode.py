e101a7ca87fa00f90b6ac6c967fb7c5c426a1d8b
Symbiosa <103031295+Symbiosa@users.noreply.github.com>
2024-06-18 22:03:31 +0300

    LeafNode

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 
 class TestHTMLNode(unittest.TestCase):
@@ -15,7 +15,13 @@ class TestHTMLNode(unittest.TestCase):
             node.PROPS_TO_HTML(),
             ' class="greeting" href="https://boot.dev"',
         )
+    def test_to_html_no_children(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
 
+    def test_to_html_no_tag(self):
+        node = LeafNode(None, "Hello, world!")
+        self.assertEqual(node.to_html(), "Hello, world!")
 
 if __name__ == "__main__":
     unittest.main()
