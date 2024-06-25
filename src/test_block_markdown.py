2b085d62d8411eec385eb6086ed86b8ee5697561
Symbiosa <103031295+Symbiosa@users.noreply.github.com>
2024-06-25 23:23:46 +0300

    Blocks

--- src/test_block_markdown.py
+++ src/test_block_markdown.py
@@ -12,17 +12,17 @@ from markdown_blocks import (
 
 
 class TestMarkdownToHTML(unittest.TestCase):
-    def test_markdownToBlocks(self):
+    def test_markdown_to_blocks(self):
         md = """
-            This is **bolded** paragraph
+This is **bolded** paragraph
 
-            This is another paragraph with *italic* text and `code` here
-            This is the same paragraph on a new line
+This is another paragraph with *italic* text and `code` here
+This is the same paragraph on a new line
 
-            * This is a list
-            * with items
-            """
-        blocks = markdownToBlocks(md)
+* This is a list
+* with items
+"""
+        blocks = markdown_to_blocks(md)
         self.assertEqual(
             blocks,
             [
@@ -32,20 +32,20 @@ class TestMarkdownToHTML(unittest.TestCase):
             ],
         )
 
-    def test_markdownToBlocks_newlines(self):
+    def test_markdown_to_blocks_newlines(self):
         md = """
-        This is **bolded** paragraph
+This is **bolded** paragraph
 
 
 
 
-        This is another paragraph with *italic* text and `code` here
-        This is the same paragraph on a new line
+This is another paragraph with *italic* text and `code` here
+This is the same paragraph on a new line
 
-        * This is a list
-        * with items
-        """
-        blocks = markdownToBlocks(md)
+* This is a list
+* with items
+"""
+        blocks = markdown_to_blocks(md)
         self.assertEqual(
             blocks,
             [
